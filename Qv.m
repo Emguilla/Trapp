bda93d1fa8e98a67fd6087f9166dca3c47e9a599
Emerick Guillaume <Emguilla@gmail.com>
2026-02-18 15:29:45 +0100

    Update Qv.m
    Added an exception resulting in Q=1 when the frequency array is empty.

--- Qv.m
+++ Qv.m
@@ -1,10 +1,12 @@
 function Q=Qv(F,T)
 %==================================================================================================================================%
-% Qv.m: Calculation of the vibration partition function as an array (v0.1)
+% Qv.m: Calculation of the vibration partition function as an array (v0.1.1)
 %==================================================================================================================================%
 % Version history:
 %   version 0.1 (14/08/2025) - Creation
 %       author: EYG
+%   version 0.1.1 (18/02/2026) - Added an exception resulting in Q=1 when the frequency array is empty.
+%       contrib: EYG
 %==================================================================================================================================%
 % args:
 %   F:  Frequency array
@@ -13,7 +15,10 @@ function Q=Qv(F,T)
 load('constant_fund.mat','kB','h')
 % The vibrational partition function is stored as an array, each value corresponding to a specific vibrational mode (to avoid
 % overflow errors)
-for p=1:length(F)
-    Q(p)=1./(1-exp(-h*F(p)/(kB*T)));
-end
+if isempty(F)
+    Q=1;
+else
+    for p=1:length(F)
+        Q(p)=1./(1-exp(-h*F(p)/(kB*T)));
+    end
 end
