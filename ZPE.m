6b18a8dbf496844f0b4ce5879bb8f037bc9bab7f
Emerick Guillaume <Emguilla@gmail.com>
2026-02-18 15:18:29 +0100

    Update ZPE.m
    Added an exception resulting in E=0 when the frequency array is empty.

--- ZPE.m
+++ ZPE.m
@@ -1,14 +1,19 @@
 function E=ZPE(nu)
 %==================================================================================================================================%
-% ZPE.m:    computation of the zero-point energy of a system using the frequencies of its vibrational mode
+% ZPE.m:    computation of the zero-point energy of a system using the frequencies of its vibrational mode (v0.1.1)
 %==================================================================================================================================%
 % Version history:
 %   version 0.1 (14/08/2025) - Creation
 %       author: EYG
+%   version 0.1.1 (18/02/2026) - Added an exception resulting in E=0 when the frequency array is empty.
+%       contrib: EYG
 %==================================================================================================================================%
 % args:
 %   nu: Array of frequencies
 %==================================================================================================================================%
 load('constant_fund.mat','h')
-E=0.5*h*sum(nu);
+if ~isempty(nu)
+    E=0.5*h*sum(nu);
+else
+    E=0;
 end
